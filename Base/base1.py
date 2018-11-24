d6318145aa4b1688ecff944c56aa8639bc4c68c4
WMM2018 <2533163607@qq.com>
2018-11-24 15:25:06 +0800

    第一次提交

---
+++ Base/base1.py
@@ -0,0 +1,10 @@
+class Base_1():
+    def __init__(self,driver):
+        self.driver =driver
+    #     查询元素
+    def get_element(self,gets,value):
+       element = self.driver.find_element(*log)
+       return element
+    # 点击元素
+    def a_click(self,log):
+        self.find_element(log)
